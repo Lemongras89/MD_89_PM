fb58b460eb6054984caa862b25de687641fce283
Lemongras89 <mplahty@yandex.ru>
2023-10-14 20:42:21 +0300

    First_commit

--- game_v2.py
+++ game_v2.py
@@ -1,26 +1,3 @@
-import numpy as np
-
-def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
-
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
-
-    Returns:
-        int: Число попыток
-    """
-
-    count = 0
-
-    while True:
-        count += 1
-        predict_number = np.random.randint(1, 112) # предполагаемое число
-        if number == predict_number:
-            break # выход из цикла, если угадали
-    return(count)
-
-print(f'Количество попыток: {random_predict()}')
-
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
 
@@ -33,7 +10,7 @@ def score_game(random_predict) -> int:
 
     count_ls = [] # список для сохранения количества попыток
     np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    random_array = np.random.randint(1, 1020, size=(1000)) # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
